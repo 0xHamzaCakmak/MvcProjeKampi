9150ef0de62049fd3b06e6d8655ee506954c614b
Hamza Çakmak <hamza.cakmak@pegitron.com>
2021-11-20 23:20:12 +0300

    a

---
+++ EntityLayer/Concrete/Abaut.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Abaut
+    {
+        [Key]
+        public int AbaoutId { get; set; }
+        [StringLength(1000)]
+        public string AbaoutDetails1 { get; set; }
+        [StringLength(1000)]
+        public string AbaoutDetails2 { get; set; }
+        [StringLength(100)]
+        public string AbaoutImage1 { get; set; }
+        [StringLength(100)]
+        public string AbaoutImage2 { get; set; }
+    }
+}
